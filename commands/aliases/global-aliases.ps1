b32bdff9876eb33973e39b724634c86ae685dee8
avnexcode <axnvee18@gmail.com>
2024-08-10 09:36:45 +0700

    message: update saja

--- commands/aliases/global-aliases.ps1
+++ commands/aliases/global-aliases.ps1
@@ -69,11 +69,3 @@ function spt {
 function obs {
     Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio\OBS Studio (64bit)"
 }
-
-function startall {
-    dc
-    spt
-    chr
-    wa
-    exp
-}
